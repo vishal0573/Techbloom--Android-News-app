8f60f071f25ab652c6a327ccca9b69d46ea95968
vishal0573 <63554189+vishal0573@users.noreply.github.com>
2020-09-01 20:55:29 +0530

    Add files via upload

---
+++ src/main/java/com/projectdev/techbloom/ApiInterface.java
@@ -0,0 +1,27 @@
+package com.projectdev.techbloom;
+
+import com.projectdev.techbloom.Model.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+    @GET("everything")
+    Call<Headlines> getSpecificData(
+            @Query("q") String query,
+            @Query("apiKey") String apiKey
+    );
+
+
+
+}
+
